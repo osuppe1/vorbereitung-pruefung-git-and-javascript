f16ecd14d159eb9390910a3704ce619bc550d281
Servan Akçay <e227637@sbl.ch>
2022-12-14 12:36:14 +0100

    seitlich begrenzt

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const circle = document.querySelector("#circ")
 let dy = 0
+let dx = 0
 
 function start() {
+
+    dx = 1
+    
     window.requestAnimationFrame(update)
 }
 
@@ -19,6 +23,17 @@ function update() {
 
     cy = cy + dy
     circle.setAttribute("cy", cy)
+
+    // Seitwärts Bewegung
+    let cx = parseFloat(circle.getAttribute("cx"))
+
+    if (cx > 100) {
+        dx = dx * (-1)
+    } else if (cx < 0)
+        dx = dx * (-1)
+
+    cx = cx + dx
+    circle.setAttribute("cx", cx)
 
     window.requestAnimationFrame(update)
 }
